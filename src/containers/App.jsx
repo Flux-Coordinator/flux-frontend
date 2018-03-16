08dc6c9b49c87f09eae0a7b3768d4430c019df0b
Esteban Luchsinger <eluchsingerm@hotmail.com>
2018-03-16 17:42:31 +0100

    Added tests and updated sonar to ignore testfile coverage

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,18 +1,16 @@
 // @flow
 import * as React from "react";
-import "./App.css";
+
 import Layout from "./Layout";
 
-class App extends React.Component<{}> {
-	render() {
-		return (
-			<div className="App">
-				<Layout>
-					<p>Hallo!</p>
-				</Layout>
-			</div>
-		);
-	}
+function App() {
+	return (
+		<div className="App">
+			<Layout>
+				<p>Hallo!</p>
+			</Layout>
+		</div>
+	);
 }
 
 export default App;
