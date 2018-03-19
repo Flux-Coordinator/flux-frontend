e1f34817ffabe5005178f9995131fb787d5d1563
Esteban Luchsinger <eluchsingerm@hotmail.com>
2018-03-19 09:03:25 +0100

    FLUX-56 Updated eslint configuration for stories

--- stories/index.js
+++ stories/index.js
@@ -1,14 +1,18 @@
 // @flow
 
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import Button from '../src/components/Button';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import Button from "../src/components/Button";
 
-storiesOf('Button', module)
-    .add('with text', () => (
-        <Button onClick={action('clicked')}>Hello Button</Button>
-    ))
-    .add('with some emoji', () => (
-        <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
-    ));
+storiesOf("Button", module)
+	.add("with text", () => (
+		<Button onClick={action("clicked")}>Hello Button</Button>
+	))
+	.add("with some emoji", () => (
+		<Button onClick={action("clicked")}>
+			<span role="img" aria-label="Nice Cool Ok 100">
+				😀 😎 👍 💯
+			</span>
+		</Button>
+	));
