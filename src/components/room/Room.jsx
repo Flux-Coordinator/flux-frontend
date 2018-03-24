e71fdb56d0a65acc0af87948dfcd1989c026d7b5
Esteban Luchsinger <eluchsingerm@hotmail.com>
2018-03-25 00:11:52 +0100

    FLUX-57 Added content for the rooms

--- src/components/room/Room.jsx
+++ src/components/room/Room.jsx
@@ -1,8 +1,17 @@
 // @flow
 import * as React from "react";
 import Heading from "grommet/components/Heading";
+import Headline from "grommet/components/Headline";
+import Header from "grommet/components/Header";
+import Title from "grommet/components/Title";
+import Columns from "grommet/components/Columns";
+import Section from "grommet/components/Section";
+import Box from "grommet/components/Box";
+import Image from "grommet/components/Image";
+import Paragraph from "grommet/components/Paragraph";
 
 import RoomModel from "../../models/Room";
+import placeholder from "../../images/placeholder.png";
 
 type Props = {
 	room: RoomModel
@@ -10,9 +19,36 @@ type Props = {
 
 export default function Room({ room }: Props) {
 	return (
-		<React.Fragment>
-			<Heading tag="h1">This is the room with the ID: {room.id}</Heading>
-			<Heading tag="h2">This is the room with the name: {room.name}</Heading>
-		</React.Fragment>
+		<Box pad="small">
+			<Section>
+				<Header size="small">
+					<Title>Beschreibung</Title>
+				</Header>
+				<Paragraph margin="none">
+					Name: {room.name ? room.name : "Raum hat keinen Namen"}
+				</Paragraph>
+				<Paragraph margin="none">
+					Beschreibung:{" "}
+					{room.description ? room.description : "Keine Beschreibung"}
+				</Paragraph>
+				<Paragraph margin="none">
+					Länge: {room.length ? room.length : "Keine Länge vorhanden"}
+				</Paragraph>
+				<Paragraph margin="none">
+					Breite: {room.width ? room.width : "Keine Breite vorhanden"}
+				</Paragraph>
+			</Section>
+			<Section>
+				<Header size="small">
+					<Title>Messungen</Title>
+				</Header>
+			</Section>
+			<Section>
+				<Header size="small">
+					<Title>Grundriss</Title>
+				</Header>
+				<Image src={placeholder} size="large" />
+			</Section>
+		</Box>
 	);
 }
