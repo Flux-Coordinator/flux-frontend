f4860b9f39c761e34cc4963cad78b4aab29e8e2d
Esteban Luchsinger <eluchsingerm@hotmail.com>
2018-03-27 08:13:50 +0200

    FLUX-57 Updated Dashboard

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,12 +1,32 @@
 // @flow
 import * as React from "react";
+import Header from "grommet/components/Header";
+import Article from "grommet/components/Article";
+import Box from "grommet/components/Box";
 import Heading from "grommet/components/Heading";
 
 export default function Welcome() {
 	return (
-		<React.Fragment>
-			<Heading tag="h1">Welcome to Flux-Coordinator!</Heading>
-			<Heading tag="h2">We are working on building something great!</Heading>
-		</React.Fragment>
+		<Article pad="medium">
+			<Header>
+				<Heading tag="h2">Dashboard</Heading>
+			</Header>
+			<Box>
+				<Box basis="1/2">
+					<Header size="small">
+						<Heading tag="h3">Systemstatus</Heading>
+					</Header>
+					<div>Platform: Cloud</div>
+					<div>Status: Bereit</div>
+					<div>
+						Sensoren:
+						<ul>
+							<li>Pozyx</li>
+							<li>Lux Meter</li>
+						</ul>
+					</div>
+				</Box>
+			</Box>
+		</Article>
 	);
 }
