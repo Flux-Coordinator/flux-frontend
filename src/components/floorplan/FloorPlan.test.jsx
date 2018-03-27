c5584f832c52a5c273f6e83e4049c3e443e82db7
Esteban Luchsinger <eluchsingerm@hotmail.com>
2018-03-27 22:01:41 +0200

    FLUX-57 Added tests for the header

--- src/components/floorplan/FloorPlan.test.jsx
+++ src/components/floorplan/FloorPlan.test.jsx
@@ -1,17 +1,25 @@
 // @flow
 import * as React from "react";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 
 import FloorPlan from "./FloorPlan";
 
 describe("<FloorPlan />", () => {
+	const placeholderText = "placeholder.png";
+	const alternativeText = "Floor Plan";
+
 	it("should render", () => {
 		const wrapper = shallow(<FloorPlan />);
 		expect(wrapper).toHaveLength(1);
 	});
 
-	it("should have a placeholder when nothing else is provided", () => {
+	it("should have a placeholder image when nothing else is provided", () => {
 		const wrapper = shallow(<FloorPlan />);
-		expect(wrapper.props().src).toBe("placeholder.png");
+		expect(wrapper.props().src).toBe(placeholderText);
+	});
+
+	it("should have a placeholder alt text when nothing else is provided", () => {
+		const wrapper = shallow(<FloorPlan />);
+		expect(wrapper.props().alt).toBe(alternativeText);
 	});
 });
---
+++ src/components/header/Header.test.jsx
@@ -0,0 +1,25 @@
+// @flow
+import * as React from "react";
+import { shallow } from "enzyme";
+import Title from "grommet/components/Title";
+import Anchor from "grommet/components/Anchor";
+
+import Header from "./Header";
+
+describe("<Header />", () => {
+	it("should render", () => {
+		const wrapper = shallow(<Header />);
+		expect(wrapper).toHaveLength(1);
+	});
+
+	it("should contain a title", () => {
+		const wrapper = shallow(<Header />);
+		expect(wrapper.contains(<Title />)).toBe(true);
+	});
+
+	it("should contain an anchor to the root page", () => {
+		const wrapper = shallow(<Header />);
+		const anchor = wrapper.find("Anchor");
+		expect(anchor.props().path).toBe("/");
+	});
+});
