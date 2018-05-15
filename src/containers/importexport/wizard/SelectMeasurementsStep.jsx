228def02ee48f0f430f6e25fb3f424756cb20872
Esteban Luchsinger <eluchsingerm@hotmail.com>
2018-05-15 13:17:45 +0200

    FLUX-141 Measurements are now shown

--- src/containers/importexport/wizard/SelectMeasurementsStep.jsx
+++ src/containers/importexport/wizard/SelectMeasurementsStep.jsx
@@ -1,8 +1,15 @@
 // @flow
 import * as React from "react";
+import Section from "grommet/components/Section";
+import Header from "grommet/components/Header";
+import Heading from "grommet/components/Heading";
+import Box from "grommet/components/Box";
 
 import WizardStep from "./WizardStep";
+import ItemsList from "./../../../components/list/ItemsList";
 import Project from "./../../../models/Project";
+import Room from "./../../../models/Room";
+import Measurement from "./../../../models/Measurement";
 
 import type { StepProps } from "./../../../containers/importexport/ExportWizard";
 
@@ -26,14 +33,42 @@ export default class SelectMeasurementsStep extends React.Component<
 		this.props.onNext(this.state.returnData);
 	};
 
+	RenderProject = (project: Project) => {
+		return project.rooms.map(r => (
+			<Section pad="none" key={r.roomId}>
+				<Header margin="none" pad="none" size="small">
+					<Heading tag="h4" strong>
+						{r.name}
+					</Heading>
+				</Header>
+				<Box>
+					<ItemsList
+						ItemRenderer={MeasurementItemRenderer}
+						items={r.measurements}
+						keyFunc={item => item.measurementId}
+						selectable={"multiple"}
+						// onSelect={selected => this.onSelect(selected, p)}
+						loading={this.props.isLoading}
+					/>
+				</Box>
+			</Section>
+		));
+	};
+
 	render() {
+		const projects = this.props.projects;
+		console.log(projects);
 		return (
 			<WizardStep
 				heading="Schritt 3: Wählen Sie die Messungen aus"
 				onNext={this.onNext}
 			>
-				<h4>Children!</h4>
+				{projects && projects.map(p => this.RenderProject(p))}
 			</WizardStep>
 		);
 	}
+}
+
+function MeasurementItemRenderer({ item }: { item: Measurement }) {
+	return item.description;
 }
