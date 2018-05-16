566b8356d09d0c5f19b5de0474d9eecc89d734a7
Esteban Luchsinger <eluchsingerm@hotmail.com>
2018-05-16 17:29:24 +0200

    Removed commented code

--- src/components/projects/AddProjectButton.jsx
+++ src/components/projects/AddProjectButton.jsx
@@ -4,6 +4,5 @@ import Anchor from "grommet/components/Anchor";
 import AddIcon from "grommet/components/icons/base/Add";
 
 export default function AddProjectButton() {
-	// return <Anchor icon={<AddIcon onClick={addProject} />} />;
 	return <Anchor icon={<AddIcon />} path="/editProject" />;
 }
