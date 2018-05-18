d746b29b967a022b685b2be7790737fcf52bce01
Patrick Scherler <pacs01dev@gmail.com>
2018-05-18 15:45:12 +0200

    change configObject types to allow partial configurations

--- src/types/Heatmap.jsx
+++ src/types/Heatmap.jsx
@@ -2,10 +2,10 @@
 
 export type ConfigObject = {
 	container?: ?Element | Text,
-	radius: ?number,
-	maxOpacity: ?number,
-	minOpacity: ?number,
-	blur: ?number
+	radius?: number,
+	maxOpacity?: number,
+	minOpacity?: number,
+	blur?: number
 };
 
 export type HeatmapDataPoint = {
