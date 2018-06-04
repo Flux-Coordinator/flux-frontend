442430668fe49d0c5732399c20e4517368b59f20
Patrick Scherler <pacs01dev@gmail.com>
2018-06-04 12:40:04 +0200

    remove search from projects list

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,8 +3,6 @@ import * as React from "react";
 import Header from "grommet/components/Header";
 import Title from "grommet/components/Title";
 import Section from "grommet/components/Section";
-import Box from "grommet/components/Box";
-import Search from "grommet/components/Search";
 
 import ContentBox from "../contentBox/ContentBox";
 import ItemsList from "./../list/ItemsList";
@@ -33,15 +31,6 @@ export default class Projects extends React.Component<Props, State> {
 				<Section>
 					<Header size="small">
 						<Title>Alle Projekte</Title>
-						<Box flex justify="end" direction="row">
-							<Search
-								inline
-								fill
-								iconAlign="start"
-								size="medium"
-								placeHolder="Projekte suchen"
-							/>
-						</Box>
 					</Header>
 				</Section>
 				<Section>
