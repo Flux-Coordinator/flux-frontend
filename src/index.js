ec43803d1588deaae6a851b82987924867f228d2
Esteban Luchsinger <eluchsingerm@hotmail.com>
2018-06-05 18:08:13 +0200

    Added swiss german locale for momentjs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
+import "moment/locale/de-ch";
 
 import "./index.css";
 import App from "./containers/app/App";
